12c2b9fb197844a1a6c663ab95690508845d974a
Brandon Rdgz <brandon.rdgz@gmail.com>
2024-11-21 20:35:18 -0600

    Initial project structure

---
+++ src/main/java/com/rodua/basic_rest_api_example/BasicRestApiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.rodua.basic_rest_api_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicRestApiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicRestApiExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodua/basic_rest_api_example/BasicRestApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodua.basic_rest_api_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicRestApiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
